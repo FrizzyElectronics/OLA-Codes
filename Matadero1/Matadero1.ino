cd02dd78c69dc3bfd505ff8fc633bc7fa42637c0
Ionut Morariu <ionut.morariu.c@gmail.com>
2018-05-03 22:03:45 +0200

    alterno()arreglado

--- Matadero1/Matadero1.ino
+++ Matadero1/Matadero1.ino
@@ -181,12 +181,13 @@ void random_led_face() {
 
 void alternos(int red, int green, int blue) {
   for (int i = 0; i < CARAS; i++) {
-    for (int j = 0; j < NUMPIXELS; j++) {
-      if (j % 2) {
-        cubo[i].setPixelColor(j, red, green, blue);
-      } else {
-        cubo[i].setPixelColor(j, 255, 255, 255);
-      }
+    for (int j = 1; j < NUMPIXELS; j += 2) {
+      cubo[i].setPixelColor(j, red, green, blue);
+    }
+  }
+  for (int i = 0; i < CARAS; i++) {
+    for (int j = 0; j < NUMPIXELS; j += 2) {
+      cubo[i].setPixelColor(j, 100, 100, 100);
     }
   }
 }
